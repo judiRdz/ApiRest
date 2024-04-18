504ff54014b37eeb3ecc6eec660c661afc8879c4
Judith Rodriguez <rodriguezsalinasperla@gmail.com>
2024-04-18 09:25:56 -0600

    Commit con todo lo nuevo

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = requiere('express');
+const port = 3002;
+
+const bodyParser = require ('body-parser');
+const routes = require ('./routes/routes')
+const app = express();
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+
+routes(app);
+
+const server = app.listen(port, (error) => {
+    if (error) return console.log (`Error: ${error}`);
+
+    console.log(`El servidor escucha en el puerto ${server.address().port}`);
+})
